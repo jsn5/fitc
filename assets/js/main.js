362e4067067b164d20604f3b3d99518565bc18be
Jaison Saji Chacko <jaison.saji@camcom.ai>
2022-11-26 00:29:01 +0530

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+}
+/*
+     FILE ARCHIVED ON 02:00:53 Aug 17, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:35:16 Nov 25, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 297.95
+  exclusion.robots: 0.1
+  exclusion.robots.policy: 0.09
+  cdx.remote: 0.081
+  esindex: 0.011
+  LoadShardBlock: 185.311 (3)
+  PetaboxLoader3.datanode: 63.669 (4)
+  CDXLines.iter: 23.931 (3)
+  PetaboxLoader3.resolve: 177.326 (2)
+  load_resource: 80.522
+*/
